f8b0ddf3b882eec87a8db36d9daf88dbd1b2b3fb
zepvalue <zepvalue@gmail.com>
2020-03-21 23:02:14 -0400

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+file = File.read("/mnt/d/Development/mc-wiki-rails/db/challenges_data.json")
+data_hash = JSON.parse(file)
+
+data_hash["activities"]["challenges"].map{ |challenge|
+  Challenge.create(
+    game_title: challenge["game_title"],
+    purpose: challenge["purpose"],
+    rules: challenge["rules"],
+    space: challenge["space"],
+    goal: challenge["goal"],
+    challenge: challenge["challenge"],
+    core_mechanics: challenge["core_mechanics"],
+    components: challenge["components"],
+  )
+}
+
+
+p "Created #{Challenge.count} challenges"
