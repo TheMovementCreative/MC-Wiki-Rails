396037cbfd2579fa18bc5488f8f0f74b5c48d550
Giuseppe Valentino <zepvalue@gmail.com>
2020-03-30 11:17:53 -0400

    ooops

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the SL available within this file, see https://guides.rubyonrails.org/routing.html
