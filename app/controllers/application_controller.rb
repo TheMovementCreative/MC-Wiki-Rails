ffbc1939b6c3c08393edd8136dee764bf2e79ab3
Giuseppe Valentino <zepvalue@gmail.com>
2020-03-29 21:31:17 -0400

    added super admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
       rescue_from CanCan::AccessDenied do |exception|
         flash[:error] = exception.message
         redirect_to root_url
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
+    can :manage, :all
     if user.superadmin_role?
       can :manage, :all
     end
     if user.supervisor_role?
       can :manage, User
+      can :access, :rails_admin       
+      can :manage, :dashboard    
     end
 
   end
