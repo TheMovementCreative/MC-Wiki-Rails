5bb85001f0fedb270f52014abe89dc36aac0aa9f
PJ <>
2020-03-31 19:38:50 -0400

    sign in page first pass

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -2,7 +2,7 @@ class DemosController < ApplicationController
     skip_before_action :authenticate_user!, :only => [:index]
 
 def index
-    @lessons = Lesson.find(15,24,32,23,21)
+    @lessons = Lesson.find(15,24,23,21)
 end
 
 end
