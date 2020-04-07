710d7caa6fc2d225bceae08b4941a6ced422247f
Giuseppe Valentino <zepvalue@gmail.com>
2020-04-07 08:47:11 -0400

    added functional components

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,23 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const HelloWorld = ({greetings}) => {
+  return (
+    <>{greetings}</>
+  )
+}
+
+// class HelloWorld extends React.Component {
+//   render () {}
+//     return (
+//       <React.Fragment>
+//         Greeting: {this.props.greeting}
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+HelloWorld.propTypes = {
+  greeting: PropTypes.string
+};
+export default HelloWorld
