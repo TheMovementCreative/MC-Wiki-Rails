dfcf54962fe8f65e1856148e2ed194457e38fb79
PJ <>
2020-04-16 20:39:05 -0400

    home.jsx axios trails

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes, { string } from "prop-types";
 import MovementDice from "./MovementDice";
 import HomeFeatueCard from "./HomeFeatureCard";
-import ChallengeInfo from "./ChallengeInfo";
 import mikePic from "../../assets/images/mike_profile_pic";
+import axios from "axios";
 
 const Home = ({ challenges, lessons }) => {
-  let featuredLesson = lessons[Math.floor(Math.random() * lessons.length)];
+  const [featuredChallenge, setFeaturedChallenge] = useState();
+  const [featuredLesson, setFeaturedLesson] = useState();
+
+
+
+  useEffect(() => {
+    const fetchFeatures = async () => {
+      const resultChallenge = await axios("/api/challenges/"+ 1);
+      const resultLesson = await axios("/api/lessons/"+ 15);
+      setFeaturedChallenge(resultChallenge.data);
+      setFeaturedLesson(resultLesson.data);
+    };
+    fetchFeatures();
+  }, []);
 
   return (
     <React.Fragment>
@@ -29,12 +42,12 @@ const Home = ({ challenges, lessons }) => {
             <header>
               <strong>Featured Daily Challenge</strong>
             </header>
-            <HomeFeatueCard
-              type={"Challenge"}
-              featuredItem={
-                challenges[Math.floor(Math.random() * challenges.length)]
-              }
-            />
+            {featuredChallenge && (
+              <HomeFeatueCard
+                type={"Challenge"}
+                featuredItem={featuredChallenge}
+              />
+            )}
           </div>
           <div
             className="col-12 col-md nest-col"
@@ -81,35 +94,30 @@ const Home = ({ challenges, lessons }) => {
                   <small>&#x25B6;</small>
                 </button>
               </div>
-              <div className="col-12 nest-col my-2 py-2 shadow-box bg-img-home-ad"
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-around",
-                    alignItems: "baseline",
-                    flexDirection: "column",
-                    borderRadius: "20px",
-                   
-                  }}
-                >
-                
-                    <h3 className="tmc-white">
-                      Don't
-                      <br /> train alone!
-                    </h3>
-                  
+              <div
+                className="col-12 nest-col my-2 py-2 shadow-box bg-img-home-ad"
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "baseline",
+                  flexDirection: "column",
+                  borderRadius: "20px",
+                }}
+              >
+                <h3 className="tmc-white">
+                  Don't
+                  <br /> train alone!
+                </h3>
 
-                 
-                    <button className="btn-sm btn-primary">
-                      {" "}
-                      <a
-                        className="tmc-white"
-                        href="mailto:info@themovementcreative.com"
-                      >
-                        Book a private today!
-                      </a>
-                    </button>
-                  
-                
+                <button className="btn-sm btn-primary">
+                  {" "}
+                  <a
+                    className="tmc-white"
+                    href="mailto:info@themovementcreative.com"
+                  >
+                    Book a private today!
+                  </a>
+                </button>
               </div>
             </div>
           </div>
@@ -124,108 +132,103 @@ const Home = ({ challenges, lessons }) => {
             <MovementDice challenges={challenges} />
           </div>
         </div>
-      
 
-      <div className="nested-row-empty row mb-3">
-        <div className="col-12 col-md-8 nest-col shadow-box">
-          <strong>Featured Weeekly Lesson</strong>
-          <div className="row nested-row">
-            <div className="col-12 col-md-8 nest-col center-spread">
-              <HomeFeatueCard type={"lesson"} featuredItem={featuredLesson} />
+        <div className="nested-row-empty row mb-3">
+          <div className="col-12 col-md-8 nest-col shadow-box">
+            <strong>Featured Weeekly Lesson</strong>
+            <div className="row nested-row">
+              {featuredLesson && <React.Fragment><div className="col-12 col-md-8 nest-col center-spread">
+                <HomeFeatueCard type={"lesson"} featuredItem={featuredLesson} />
+              </div>
+              <div className="col-12 col-md-3 nest-col">
+                <div className="row nested-row">
+                  <div className="col-6 col-md-12 nest-col">
+                    <h4>Author:</h4>
+                    <p>{featuredLesson.author}</p>
+                  </div>
+                  <div className="col-5 col-md-12 nest-col ">
+                    <img src={mikePic} />
+                  </div>
+                  <div className="col-12 nest-col">
+                    <hr />
+                    <h4>Activities included: </h4>
+                    <p>
+                      {featuredLesson.details !== null &&
+                        featuredLesson.details.slice(0, 100)}
+                      ...
+                    </p>
+                  </div>
+                </div>
+              </div> </React.Fragment>}
             </div>
-            <div className="col-12 col-md-3 nest-col">
-              <div className="row nested-row">
-                <div className="col-6 col-md-12 nest-col">
-                  <h4>Author:</h4>
-                  <p>{featuredLesson.author}</p>
-                </div>
-                <div className="col-5 col-md-12 nest-col ">
-                  <img src={mikePic} />
-                </div>
-                <div className="col-12 nest-col">
-                  <hr />
-                  <h4>Activities included: </h4>
-                  <p>
-                    {featuredLesson.details !== null &&
-                      featuredLesson.details.slice(0, 100)}
-                    ...
-                  </p>
-                </div>
+          </div>
+
+          <div className="col-12 col-md nest-col">
+            <div className="row nested-row-empty container-fluid center-spread">
+              <div
+                className="col-12 shadow-box"
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  borderRadius: "15px",
+                }}
+              >
+                <strong>Select from {lessons.length} unique Lessons!</strong>
+                <button
+                  className="btn-lg tmc-bg-blu tmc-white my-2"
+                  href="/lessons"
+                >
+                  Browse <small>&#x25B6;</small>
+                  <small>&#x25B6;</small>
+                  <small>&#x25B6;</small>
+                </button>
+              </div>
+
+              <div
+                className="col-12 shadow-box bg-img-home-ad-mj my-2"
+                style={{
+                  minHeight: "100px",
+                  borderRadius: "15px",
+                  display: "flex",
+                  alignItems: "flex-end",
+                }}
+              >
+                <button className="btn-sm btn-primary mb-2">
+                  {" "}
+                  Online Classses <small>&#x25B6;</small>
+                </button>
+              </div>
+
+              <div
+                className="col-12 shadow-box bg-img-home-ad-wt mb-2"
+                style={{ minHeight: "100px", borderRadius: "15px" }}
+              >
+                <button className="btn-sm btn-primary mt-2">
+                  Visit Our Store <small>&#x25B6;</small>
+                </button>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="col-12 col-md nest-col">
-          <div
-            className="row nested-row-empty container-fluid center-spread"
-       
-          >
-            <div
-              className="col-12 shadow-box"
-              style={{
-                display: "flex",
-                justifyContent: "space-around",
-                alignItems: "center",
-                flexDirection: "column",
-                borderRadius: "15px",
-              }}
-            >
-              <strong>Select from {lessons.length} unique Lessons!</strong>
-              <button
-                className="btn-lg tmc-bg-blu tmc-white my-2"
-                href="/lessons"
-              >
-                Browse <small>&#x25B6;</small>
-                <small>&#x25B6;</small>
-                <small>&#x25B6;</small>
-              </button>
-            </div>
+        <div className="container-fluid row nested-row mb-3">
+          <div className="col-6 shadow-box">
+            <strong>Start a course</strong>
+            <HomeFeatueCard type={"course"} />
+          </div>
 
-            <div
-              className="col-12 shadow-box bg-img-home-ad-mj my-2"
-              style={{
-                minHeight: "100px",
-                borderRadius: "15px",
-                display: "flex",
-                alignItems: "flex-end",
-              }}
-            >
-              <button className="btn-sm btn-primary mb-2">
-                {" "}
-                Online Classses <small>&#x25B6;</small>
-              </button>
-            </div>
-
-            <div
-              className="col-12 shadow-box bg-img-home-ad-wt mb-2"
-              style={{ minHeight: "100px", borderRadius: "15px" }}
-            >
-              <button className="btn-sm btn-primary mt-2">
-                Visit Our Store <small>&#x25B6;</small>
-              </button>
-            </div>
+          <div className="col-6 shadow-box box-rnd">
+            <strong>Courses Include</strong>
+            <hr />
+            <p>Progress tracker (so you don't have to).</p>
+            <p>A week of training that you can use over and over again!</p>
+            <p>
+              Feedback from a member of the Movement Creative Coaching Staff
+            </p>
           </div>
         </div>
-      </div>
-
-      <div className="container-fluid row nested-row mb-3">
-        <div
-          className="col-6 shadow-box"
-          
-        >
-          <strong>Start a course</strong>
-          <HomeFeatueCard type={"course"} />
-        </div>
-
-        <div className="col-6 shadow-box box-rnd">
-          <strong>Courses Include</strong>
-          <hr />
-          <p>Progress tracker (so you don't have to).</p>
-          <p>A week of training that you can use over and over again!</p>
-          <p>Feedback from a member of the Movement Creative Coaching Staff</p>
-        </div>
-      </div>
       </div>
     </React.Fragment>
   );
