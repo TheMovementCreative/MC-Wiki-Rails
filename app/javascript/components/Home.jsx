79a2905c7bbaa3e15a14a6e9820b61110f5acd06
PJRandol <randolpeter@gmail.com>
2020-05-07 16:27:18 -0400

    env vars

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -5,14 +5,15 @@ import HomeFeatueCard from "./HomeFeatureCard";
 import mikePic from "../../assets/images/mike_profile_pic";
 import * as Constants from '../constants';
 import axios from "axios";
+require('dotenv').config()
 
 const Home = ({ challenges, lessons, page }) => {
   const [featuredChallenge, setFeaturedChallenge] = useState();
   const [featuredLesson, setFeaturedLesson] = useState();
 
   const fetchFeatures = async () => {
-    const resultChallenge = await axios(Constants.BASEURL+"/api/challenges/"+( page.featured_challenge_id));
-    const resultLesson = await axios(Constants.BASEURL+"/api/lessons/"+ (page.featured_lesson_id));
+    const resultChallenge = await axios(Constants.BASEURLPRO+"/api/challenges/"+( page.featured_challenge_id));
+    const resultLesson = await axios((REACT_APP_BASE_URL || Constants.BASEURL) +"/api/lessons/"+ (page.featured_lesson_id));
     setFeaturedChallenge(resultChallenge.data);
     setFeaturedLesson(resultLesson.data);
   };
--- app/javascript/constants/index.js
+++ app/javascript/constants/index.js
@@ -1,3 +1,5 @@
-export const BASEURL = "http://lit-badlands-45841.herokuapp.com"
+export const BASEURL = "localhost:3000"
+export const BASEURLPRO = REACT_APP_BASE_URL || "localhost:3000"
+
 
 //comment
