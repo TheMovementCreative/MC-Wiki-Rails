b258a5a5db37fdd52c5610e26beac9739175e415
PJRandol <randolpeter@gmail.com>
2020-06-24 10:46:34 -0400

    sign ups, mailers -- forgot pw

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
@@ -31,9 +33,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # devise mailer setup
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
+  #devise mailer
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
